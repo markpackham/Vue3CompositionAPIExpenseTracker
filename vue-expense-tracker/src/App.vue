4924cdf866a19192ac5770dc250aaa275ccc9ab9
mark <markpackham1@gmail.com>
2023-11-22 12:28:28 +0000

    add Balance

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
   <div class="container">
+    <Balance />
   </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
 </script>
