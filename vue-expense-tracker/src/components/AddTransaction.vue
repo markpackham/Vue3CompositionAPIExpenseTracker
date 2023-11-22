ab81e2323f674554cf12bf4c257e27dbda5f8745
mark <markpackham1@gmail.com>
2023-11-22 17:44:59 +0000

    add v-model for form input fields
    https://youtu.be/hNPwdOZ3qFU?feature=shared&t=2742

--- vue-expense-tracker/src/components/AddTransaction.vue
+++ vue-expense-tracker/src/components/AddTransaction.vue
@@ -3,12 +3,12 @@
     <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..." />
+            <input type="text" id="text" v-model="text" placeholder="Enter text..." />
         </div>
         <div class="form-control">
             <label for="amount">Amount <br />
                 (negative - expense, positive - income)</label>
-            <input type="number" id="amount" placeholder="Enter amount..." />
+            <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
         </div>
         <button class="btn">Add transaction</button>
     </form>
