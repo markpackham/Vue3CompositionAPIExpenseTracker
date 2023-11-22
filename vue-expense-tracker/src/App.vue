0be6392457a567427c53c947c62a05c119700ff4
mark <markpackham1@gmail.com>
2023-11-22 14:02:54 +0000

    add dummy data for TransactionList
    https://youtu.be/hNPwdOZ3qFU?feature=shared&t=1292

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -4,6 +4,7 @@
     <Balance />
     <IncomeExpenses />
     <TransactionList />
+    <AddTransaction />
   </div>
 </template>
 
@@ -12,4 +13,5 @@ import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 </script>
--- vue-expense-tracker/src/components/TransactionList.vue
+++ vue-expense-tracker/src/components/TransactionList.vue
@@ -1,11 +1,26 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
+        <!-- <li class="minus">
             Cash <span>-£400</span><button class="delete-btn">x</button>
         </li>
         <li class="plus">
             Paycheck <span>£800</span><button class="delete-btn">x</button>
-        </li>
+        </li> -->
     </ul>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            transactions: [
+                { id: 1, text: 'Flower', amount: -19.99 },
+                { id: 2, text: 'Salary', amount: 229.99 },
+                { id: 3, text: 'Book', amount: -10.99 },
+                { id: 4, text: 'Tip', amount: 19.99 },
+            ]
+        }
+    }
+}
+</script>
