38bf6fb1aa4a8193467093ed270aafe83eb462cf
mark <markpackham1@gmail.com>
2023-11-22 12:46:33 +0000

    add TransactionList

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -2,10 +2,14 @@
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpenses />
+    <TransactionList />
   </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
 </script>
